79f5e95a43cecea961586f8acf9a71bd6e89273a
sunghyun.kang <sunghyun.kang@nhnpayco.com>
2020-01-13 18:03:13 +0900

    #User로직 작성

---
+++ src/main/java/com/nhn/review/user/entity/User.java
@@ -0,0 +1,13 @@
+package com.nhn.review.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+}
