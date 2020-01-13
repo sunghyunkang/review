551fceeaee6b13ff844034a5e8be12809e9c441f
sunghyun.kang <sunghyun.kang@nhnpayco.com>
2020-01-13 18:26:08 +0900

    #Anagram Stream만으로 구현

--- src/main/java/com/nhn/review/quiz/Anagrams.java
+++ src/main/java/com/nhn/review/quiz/Anagrams.java
@@ -2,33 +2,33 @@ package com.nhn.review.quiz;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class Anagrams {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        File dictionary = new File(args[0]);
+    public static void main(String[] args) throws IOException {
+        Path dictionary = Paths.get(args[0]);
         int minGroupSize = Integer.parseInt(args[1]);
 
-        Map<String, Set<String>> groups = new HashMap<>();
-        try(Scanner s = new Scanner(dictionary)){
-            while(s.hasNext()){
-                String word = s.next();
-                groups.computeIfAbsent(alphabetize(word),
-                        (unused) -> new TreeSet<>()).add(word);
-            }
-        }
-
-        for(Set<String> group : groups.values()){
-            if(group.size() >= minGroupSize)
-                System.out.println(group.size() + " : " + group);
+        try(Stream<String> words = Files.lines(dictionary)){
+            words.collect(
+                    groupingBy(word -> word.chars().sorted()
+                            .collect(StringBuilder::new,
+                                    (sb, c) -> sb.append((char) c),
+                                    StringBuilder::append).toString()))
+                    .values().stream()
+                    .filter(group -> group.size() >= minGroupSize)
+                    .map(group -> group.size() + " : " + group)
+                    .forEach(System.out::println);
         }
     }
 
 
-    private static String alphabetize(String s){
-        char[] a = s.toCharArray();
-        Arrays.sort(a);
-        return new String(a);
-    }
 }
