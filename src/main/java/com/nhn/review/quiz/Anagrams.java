a90e67950a86d50c7ef0c703ec37f21f938227cc
sunghyun.kang <sunghyun.kang@nhnpayco.com>
2020-01-15 14:17:57 +0900

    #Anagram 로직구현
    #Anagram Stream만으로 구현
    
    
    #Anagram Stream 적당히 사용

---
+++ src/main/java/com/nhn/review/quiz/Anagrams.java
@@ -0,0 +1,33 @@
+package com.nhn.review.quiz;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.groupingBy;
+
+public class Anagrams {
+
+    public static void main(String[] args) throws IOException {
+        Path dictionary = Paths.get(args[0]);
+        int minGroupSize = Integer.parseInt(args[1]);
+
+        try(Stream<String> words = Files.lines(dictionary)){
+            words.collect(groupingBy(word -> alphabetize(word)))
+                    .values().stream()
+                    .filter(group -> group.size() >= minGroupSize)
+                    .forEach(group -> System.out.println(group.size() + ": " + group));
+        }
+    }
+
+    private static String alphabetize(String s){
+        char[] a = s.toCharArray();
+        Arrays.sort(a);
+        return new String(a);
+    }
+}
